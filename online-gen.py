ac2bf77bc72365646ce2c4f275d75a9268bad0eb
Leo <72922793+Leorjph@users.noreply.github.com>
2026-07-10 01:08:30 -0400

    Update online-gen.py
    Added help section

--- online-gen.py
+++ online-gen.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from generate import parse_text, create_doc
+from generate import parse_text, create_doc, image_path, TAGS
 from io import BytesIO, StringIO
 
 st.title("Text to Docx Menu Generator")
@@ -49,3 +49,32 @@ if st.button("Generate Menu"):
         )
     else:
         st.error("Please upload a text file first")
+
+
+with st.sidebar.expander("? - Help"):
+    st.subheader("Text file format")
+    st.write("Each item consists for 4 lines-\n")
+    st.write("1. Item name")
+    st.write("Ingredients")
+    st.write("Tags (Comma-separated)")
+    st.write("Allergen note")
+    st.write("\n Use '-' in any column other than name to leave it empty\n\n")
+    
+    st.subheader("Tags List")
+    tags, icons = st.columns(2)
+    
+    with col1:
+        st.subheader("Tag")
+        st.write('Vegan / v')
+        st.write('Vegetarian / veg')
+        st.write('Eat well / EW / MB')
+        st.write('Halal / H')
+        st.write('Low carbon / LC / FE')
+        
+    with col2:
+        st.subheader("Icon")
+        st.image(image_path + TAGS['v'])
+        st.image(image_path + TAGS['veg'])
+        st.image(image_path + TAGS['ew'])
+        st.image(image_path + TAGS['h'])
+        st.image(image_path + TAGS['lc'])
